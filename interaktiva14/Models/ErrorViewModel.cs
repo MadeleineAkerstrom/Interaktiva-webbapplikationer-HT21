f29bb960437edc42f4637f03641bec916df9b933
Madeleine Åkerström <73692559+MadeleineAkerstrom@users.noreply.github.com>
2021-10-25 10:15:36 +0200

    Add project files.

---
+++ interaktiva14/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace interaktiva14.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
