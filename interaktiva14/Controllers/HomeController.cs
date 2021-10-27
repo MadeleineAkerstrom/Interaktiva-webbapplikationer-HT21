25579335fbe1aab6b8655dc87a77235381ff6930
Madeleine Åkerström <73692559+MadeleineAkerstrom@users.noreply.github.com>
2021-10-27 19:29:56 +0200

    Added 'task3' to implement top4 in HomeController

--- interaktiva14/Controllers/HomeController.cs
+++ interaktiva14/Controllers/HomeController.cs
@@ -16,22 +16,26 @@ namespace interaktiva14.Controllers
         private IOmdbRepository omdbRepository;
         private ICmdbRepository cmdbRepository;
 
-        public HomeController(IOmdbRepository omdbRepository)
+        public HomeController(IOmdbRepository omdbRepository, ICmdbRepository cmdbRepository)
         {
             this.omdbRepository = omdbRepository;
+            this.cmdbRepository = cmdbRepository;
         }
+       
         public async Task<IActionResult> Index()
         {
             try
             {
-                 string movieName = "Dune"; // Behöver koppla denna till html interface
+                string movieName = "Dune"; // Behöver koppla denna till html interface
                 var task1 = omdbRepository.GetMovieBySearchAsync(movieName);
                 var task2 = omdbRepository.GetMovieByTitleAsync(movieName);
+                var task3 = cmdbRepository.GetMovieToplist();
 
-                await Task.WhenAll(task1, task2); // Väntar till alla uppgifter har kört klart. 
+                await Task.WhenAll(task1, task2, task3); // Väntar till alla uppgifter har kört klart. 
                 
                 var searchResult = await task1;
                 var movieInfo = await task2;
+                var top4 = await task3;
 
                 var model = new HomeViewModel(searchResult, movieInfo);
                 return View(model);
