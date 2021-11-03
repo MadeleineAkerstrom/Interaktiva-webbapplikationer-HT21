464adf2d6c96ac2dfafdcff41965a7c2e13b359e
Jonathan Jonsson <jonsson.jonathan@hotmail.com>
2021-11-03 12:29:39 +0100

    Changed like dislike buttons to bootstrap Changed so LikeFunction takes in imdbId

--- interaktiva14/wwwroot/js/likeDislike.js
+++ interaktiva14/wwwroot/js/likeDislike.js
@@ -1,3 +1,17 @@
-function myFunction() {
-    alert("I am an alert box!");
+function LikeFunction(imdbId) {
+    likeButton = document.getElementById("likebutton0")
+    likeButton.disabled = true
+    //likeButton.style.background='#000000'
+    
+    url = "https://grupp9.dsvkurs.miun.se/api/Movie/"+imdbId+"/like"
+    fetch(url).
+    then(response => response.json())
+    .then(data => console.log(data)).finally(() => (likeButton.disabled = false));
   }
+
+function DislikeFunction() {
+    url = "https://grupp9.dsvkurs.miun.se/api/Movie/tt0111161/dislike"
+    fetch(url)
+.then(response => response.json())
+.then(data => console.log(data));
+}
