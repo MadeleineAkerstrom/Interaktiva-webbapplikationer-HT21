c945b676893739f3f87fbbc6bd641e6e18bb7690
Jonathan Jonsson <jonsson.jonathan@hotmail.com>
2021-11-03 13:04:56 +0100

    Added disable to like and dislike buttons when fetch i running

--- interaktiva14/wwwroot/js/likeDislike.js
+++ interaktiva14/wwwroot/js/likeDislike.js
@@ -1,5 +1,5 @@
-function LikeFunction(imdbId) {
-    likeButton = document.getElementById("likebutton0")
+function LikeFunction(imdbId, id) {
+    likeButton = document.getElementById("likebutton" + id)
     document.getElementsByClassName('number-of-likes-0').innerHTML = 'test'
 
     likeButton.disabled = true
@@ -12,13 +12,13 @@ function LikeFunction(imdbId) {
     
   }
 
-function DislikeFunction(imdbId) {
-    likeButton = document.getElementById("likebutton0")
-    likeButton.disabled = true
+function DislikeFunction(imdbId, id) {
+    dislikeButton = document.getElementById("dislikebutton" + id)
+    dislikeButton.disabled = true
     //likeButton.style.background='#000000'
     
     url = "https://grupp9.dsvkurs.miun.se/api/Movie/"+imdbId+"/dislike"
     fetch(url).
     then(response => response.json())
-    .then(data => console.log(data)).finally(() => (likeButton.disabled = false));
+    .then(data => console.log(data)).finally(() => (dislikeButton.disabled = false));
   }
